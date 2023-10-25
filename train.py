ec9fd2f0ff10da17c7c39185e18df80d4ef6ce5a
Navya Malhotra <navya0807@gmail.com>
2023-10-25 09:39:55 +0530

    clear

--- train.py
+++ train.py
@@ -80,10 +80,7 @@ class ChatDataset(Dataset):
         return self.n_samples
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
+train_loader = DataLoader(dataset=dataset,batch_size=batch_size,shuffle=True,num_workers=0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
